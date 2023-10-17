72a0168a17f7475d6ab3be9c766da60689694d73
Takezo Kensei <Takezo316@users.noreply.github.com>
2023-10-17 16:18:19 -0500

    Update habitica_register.js

--- Taller 2/1-Habitica/cypress/integration/habitica_register.js
+++ Taller 2/1-Habitica/cypress/integration/habitica_register.js
@@ -4,7 +4,7 @@ context('Usuario ya existe', () => {
       cy.visit('https://habitica.com/static/home')
 
       cy.get('#usernameInput').type('Takezo316');
-      cy.contains("Este nombre de usuario ya está cogido").should('be.visible')
+      cy.contains("Este nombre de usuario ya está en uso").should('be.visible')
       cy.get('input[type=email]').type('takezo316@gmail.com')
       cy.get('input[type=password]').first().type('TK316Habitica')
       cy.get('input[type=password]').last().type('TK316Habitica')
